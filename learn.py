79e65f64d32eaa643f85c8460b4faf926f4c5331
Alvaro Torralba <alto@cs.aau.dk>
2023-04-07 12:00:49 +0200

    Gen training data

--- learn.py
+++ learn.py
@@ -66,14 +66,26 @@ def main():
 
     run_step_good_operators(f'{TRAINING_DIR}/good-operators-unit', REPO_GOOD_OPERATORS, ['--search', "sbd(store_operators_in_optimal_plan=true, cost_type=1)"], ENV, SUITE_TRAINING, fetch_everything=True,)
 
-    time_limit = 10
-    memory_limit = 10000
 
-    os.mkdir(f"{TRAINING_DIR}/partial-grounding-rules")
-    Call([sys.executable, f'{REPO_LEARNING}/learning-sklearn/generate-exhaustive-feature-rules.py', args.domain, '--runs', f'{TRAINING_DIR}/good-operators-unit', '--rule_size', '7', '--store_rules', f'{TRAINING_DIR}/partial-grounding-rules/rules-exhaustive-10k', '--num_rules','10000'], "generate-rules", time_limit=time_limit, memory_limit=memory_limit).wait()
+
+
+    #run_step_partial_grounding
+    time_limit = 300
+    memory_limit = 4*1024*1024
+
+    os.mkdir(f"{TRAINING_DIR}/partial-grounding-rules")    # TODO: Set to 10k instead of 1k
+    Call([sys.executable, f'{REPO_LEARNING}/learning-sklearn/generate-exhaustive-feature-rules.py', args.domain, '--runs', f'{TRAINING_DIR}/good-operators-unit', '--rule_size', '7', '--store_rules', f'{TRAINING_DIR}/partial-grounding-rules/rules-exhaustive-1k', '--num_rules','1000'], "generate-rules", time_limit=time_limit, memory_limit=memory_limit).wait()
     # TODO: Check if rules have been correctly generated. Otherwise, re-generate with smaller size?
 
-    Call([sys.executable, f'{REPO_LEARNING}/learning-sklearn/filter-irrelevant-rules.py', '--instances-relevant-rules', '10', f'{TRAINING_DIR}/good-operators-unit', f'{TRAINING_DIR}/partial-grounding-rules/rules-exhaustive-10k', f'{TRAINING_DIR}/partial-grounding-rules/rules-exhaustive-10k-filtered'], "filter-rules", time_limit=time_limit, memory_limit=memory_limit).wait()
+    Call([sys.executable, f'{REPO_LEARNING}/learning-sklearn/filter-irrelevant-rules.py', '--instances-relevant-rules', '10', f'{TRAINING_DIR}/good-operators-unit', f'{TRAINING_DIR}/partial-grounding-rules/rules-exhaustive-1k', f'{TRAINING_DIR}/partial-grounding-rules/rules-exhaustive-1k-filtered'], "filter-rules", time_limit=time_limit, memory_limit=memory_limit).wait()
+
+    Call([sys.executable, f'{REPO_LEARNING}/learning-sklearn/generate-training-data.py', \
+                                         f'{TRAINING_DIR}/good-operators-unit',\
+                                         f'{TRAINING_DIR}/partial-grounding-rules/rules-exhaustive-1k-filtered',\
+                                         f'{TRAINING_DIR}/partial-grounding-rules/training-data-good-operators-exhaustive-1k-filtered',\
+                                         '--op-file', 'good_operators',\
+                                         '--max-training-examples', '1000000' # '--num-test-instances TODO Set some test instances
+          ], "generate-training-data-1", time_limit=time_limit, memory_limit=memory_limit).wait()
 
 
     # Call([sys.executable, f'{REPO_LEARNING}/learning-sklearn/generate-random-feature-rules.py'],  time_limit=time_limit, mem_limit=memory_limit).wait()
