89caa20a2c31b086792994204e7d2f90e44890e1
Daniel Gnad <daniel.gnad@liu.se>
2023-04-24 20:27:43 +0200

    add GoodBadRuleEvaluator to IPC queues

--- fd-partial-grounding/src/translate/subdominization/ipc23_queue.py
+++ fd-partial-grounding/src/translate/subdominization/ipc23_queue.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os.path
 
+from .good_bad_rule_evaluator import GodBadRuleEvaluator
 from .priority_queue import SortedHeapQueue, FIFOQueue
 from .queue_factory import PriorityQueue
 from .model import HybridModel
@@ -28,6 +29,10 @@ class IPC23SingleQueue(PriorityQueue):
         self.batch_eval = args.batch_evaluation
         self.non_evaluated_actions = defaultdict(list)
 
+        self.good_bad_rule_evaluator = GodBadRuleEvaluator(task, args)
+        self.good_actions = []
+        self.num_grounded_bad_actions = defaultdict(int)
+
     def __bool__(self):
         return bool(self.queue) or \
             (self.batch_eval and any(bool(actions) for actions in self.non_evaluated_actions.values()))
@@ -43,6 +48,9 @@ class IPC23SingleQueue(PriorityQueue):
 
     def print_stats(self):
         self.model.print_stats()
+        self.good_bad_rule_evaluator.print_stats()
+        for schema, num in self.num_grounded_bad_actions.items():
+            print(f"Grounded {num} bad actions from action schema {schema}")
 
     def get_hard_action_if_exists(self, is_hard_action):
         raise
@@ -50,27 +58,42 @@ class IPC23SingleQueue(PriorityQueue):
     def notify_new_hard_actions(self, schemas):
         raise
 
+    def has_good_actions(self):
+        return bool(self.good_actions)
+
     def push(self, action):
-        if self.batch_eval:
-            self.non_evaluated_actions[action.predicate.name].append(action)
+        if self.good_bad_rule_evaluator.is_bad_action(action):
+            self.queue.push(action, -inf)
+        elif self.good_bad_rule_evaluator.is_good_action(action):
+            self.good_actions.append(action)
         else:
-            estimate = self.model.get_estimate(action)
-            if estimate is None:
-                # give high probability if we don't have a model
-                estimate = 1.0
-            self.queue.push(action, estimate)
+            if self.batch_eval:
+                self.non_evaluated_actions[action.predicate.name].append(action)
+            else:
+                estimate = self.model.get_estimate(action)
+                if estimate is None:
+                    # give high probability if we don't have a model
+                    estimate = 1.0
+                self.queue.push(action, estimate)
 
     def pop(self):
+        if self.good_actions:
+            action = self.good_actions.pop()
+            self.closed.append(action)
+            return action
+
         if self.batch_eval:
             for schema, actions in self.non_evaluated_actions.items():
                 if actions:
                     estimates = self.model.get_estimates(actions)
                     if not estimates:
                         # give high probability if we don't have a model
-                        estimates = [1.0 for i in range(len(actions))]
+                        estimates = [1.0 for _ in range(len(actions))]
                     self.queue.push_list(actions, estimates)
                     self.non_evaluated_actions[schema] = []
-        action = self.queue.pop()
+        estimate, action = self.queue.pop_entry()
+        if estimate == -inf:
+            self.num_grounded_bad_actions[action.predicate.name] += 1
         self.closed.append(action)
         return action
 
@@ -89,6 +112,11 @@ class IPC23RoundRobinQueue(PriorityQueue):
             self.non_evaluated_actions = [[] for _ in self.schemas]
         self.has_actions = False
 
+        self.good_bad_rule_evaluator = GodBadRuleEvaluator(task, args)
+        self.good_actions = []
+        self.bad_actions = []
+        self.num_grounded_bad_actions = defaultdict(int)
+
     def __bool__(self):
         return self.has_actions
     __nonzero__ = __bool__
@@ -104,8 +132,11 @@ class IPC23RoundRobinQueue(PriorityQueue):
 
     def print_stats(self):
         self.model.print_stats()
+        self.good_bad_rule_evaluator.print_stats()
         for i in range(len(self.num_grounded_actions)):
             print(f"{self.num_grounded_actions[i]} actions grounded for schema {self.schemas[i]}")
+        for schema, num in self.num_grounded_bad_actions.items():
+            print(f"Grounded {num} bad actions from action schema {schema}")
 
     def get_hard_action_if_exists(self, is_hard_action):
         pass
@@ -113,30 +144,38 @@ class IPC23RoundRobinQueue(PriorityQueue):
     def notify_new_hard_actions(self):
         pass
 
+    def has_good_actions(self):
+        return bool(self.good_actions)
+
     def push(self, action):
         self.has_actions = True
-        schema = action.predicate.name
-        if schema not in self.schemas:
-            # first time we see this action schema; init datastructures
-            # TODO think of a more efficient way to implement this;
-            # can we obtain all schemas from the task initially?
-            self.schemas.append(schema)
-            self.num_grounded_actions.append(0)
-            self.queues.append(FIFOQueue())
-            self.queues[-1].push(action)
+        if self.good_bad_rule_evaluator.is_bad_action(action):
+            self.bad_actions.append(action)
+        elif self.good_bad_rule_evaluator.is_good_action(action):
+            self.good_actions.append(action)
         else:
-            index = self.schemas.index(schema)
-            if index < self.num_trained_schemas:
-                # we have a model
-                if self.batch_eval:
-                    self.non_evaluated_actions[index].append(action)
-                else:
-                    estimate = self.model.get_estimate(action)
-                    assert estimate is not None
-                    self.queues[index].push(action, estimate)
+            schema = action.predicate.name
+            if schema not in self.schemas:
+                # first time we see this action schema; init datastructures
+                # TODO think of a more efficient way to implement this;
+                # can we obtain all schemas from the task initially?
+                self.schemas.append(schema)
+                self.num_grounded_actions.append(0)
+                self.queues.append(FIFOQueue())
+                self.queues[-1].push(action)
             else:
-                # we don't have a trained model
-                self.queues[index].push(action)
+                index = self.schemas.index(schema)
+                if index < self.num_trained_schemas:
+                    # we have a model
+                    if self.batch_eval:
+                        self.non_evaluated_actions[index].append(action)
+                    else:
+                        estimate = self.model.get_estimate(action)
+                        assert estimate is not None
+                        self.queues[index].push(action, estimate)
+                else:
+                    # we don't have a trained model
+                    self.queues[index].push(action)
 
     def _evaluate_batch_schema(self, index):
         actions = self.non_evaluated_actions[index]
@@ -146,11 +185,20 @@ class IPC23RoundRobinQueue(PriorityQueue):
             self.non_evaluated_actions[index] = []
 
     def _check_has_action(self):
-        self.has_actions = any(q for q in self.queues) or \
+        self.has_actions = self.bad_actions or any(q for q in self.queues) or \
                            (self.batch_eval and any(q for q in self.non_evaluated_actions))
 
     def pop(self):
+        if self.good_actions:
+            action = self.good_actions.pop()
+            self.closed.append(action)
+            self.num_grounded_actions[self.schemas.index(action.predicate.name)] += 1
+            if not self.good_actions:
+                self._check_has_action()
+            return action
+
         self.has_actions = False
+        start = self.current
         while True:
             self.current = (self.current + 1) % len(self.schemas)
             if self.batch_eval and self.current < self.num_trained_schemas:
@@ -166,6 +214,13 @@ class IPC23RoundRobinQueue(PriorityQueue):
                     # Only add to closed if there is no model for this schema,
                     # as these actions are kept in the corresponding FIFOQueue.
                     self.closed.append(action)
+                return action
+            if self.current == start:
+                # all other queues are empty
+                action = self.bad_actions.pop()
+                self.closed.append(action)
+                if self.bad_actions:
+                    self.has_actions = True
                 return action
 
 
@@ -202,8 +257,15 @@ class IPC23RatioQueue(PriorityQueue):
         if self.batch_eval:
             self.non_evaluated_actions = [[] for _ in self.schemas]
 
+        self.good_bad_rule_evaluator = GodBadRuleEvaluator(task, args)
+        self.good_actions = []
+        self.bad_actions = []
+        self.num_grounded_bad_actions = defaultdict(int)
+
     def __bool__(self):
-        return any(q for q in self.queues) or (self.batch_eval and any(q for q in self.non_evaluated_actions))
+        return any(q for q in self.queues) or \
+            bool(self.bad_actions) or \
+            (self.batch_eval and any(q for q in self.non_evaluated_actions))
     __nonzero__ = __bool__
 
     def get_final_queue(self):
@@ -217,11 +279,14 @@ class IPC23RatioQueue(PriorityQueue):
 
     def print_stats(self):
         self.model.print_stats()
+        self.good_bad_rule_evaluator.print_stats()
         for i in range(len(self.num_grounded_actions)):
             print(f"{self.num_grounded_actions[i]} actions grounded for schema {self.schemas[i]}; "
-                  f"target ratio: {self.target_ratios[i]}, final ratio: {self.ratios[i]}")
+                  f"target ratio: {round(self.target_ratios[i], 2)}, final ratio: {round(self.ratios[i], 2)}")
         for schema in self.skipped_action_schemas:
             print(f"WARNING: Action schema {schema} did not appear in given ratios file, so was pruned completely.")
+        for schema, num in self.num_grounded_bad_actions.items():
+            print(f"Grounded {num} bad actions from action schema {schema}")
 
     def get_hard_action_if_exists(self, is_hard_action):
         pass
@@ -229,24 +294,47 @@ class IPC23RatioQueue(PriorityQueue):
     def notify_new_hard_actions(self):
         pass
 
+    def has_good_actions(self):
+        return bool(self.good_actions)
+
     def push(self, action):
-        schema = action.predicate.name
-        if schema not in self.schemas:
-            self.skipped_action_schemas.add(schema)
-            return
-        index = self.schemas.index(schema)
-        if schema not in self.trained_schemas:
-            self.queues[index].push(action)
+        if self.good_bad_rule_evaluator.is_bad_action(action):
+            self.bad_actions.append(action)
+        elif self.good_bad_rule_evaluator.is_good_action(action):
+            self.good_actions.append(action)
         else:
-            if self.batch_eval:
-                self.non_evaluated_actions[index].append(action)
+            schema = action.predicate.name
+            if schema not in self.schemas:
+                self.skipped_action_schemas.add(schema)
+                return
+            index = self.schemas.index(schema)
+            if schema not in self.trained_schemas:
+                self.queues[index].push(action)
             else:
-                self.queues[index].push(action, self.model.get_estimate(action))
+                if self.batch_eval:
+                    self.non_evaluated_actions[index].append(action)
+                else:
+                    self.queues[index].push(action, self.model.get_estimate(action))
 
     def pop(self):
-        next_index = max([(self.target_ratios[i] - self.ratios[i], i)
-                          if self.queues[i] or (self.batch_eval and self.non_evaluated_actions[i]) else (-inf, i)
-                          for i in range(len(self.ratios))], key=lambda item: item[0])[1]
+        if self.good_actions:
+            action = self.good_actions.pop()
+            self.closed.append(action)
+            self.num_grounded_actions[self.schemas.index(action.predicate.name)] += 1
+            return action
+
+        prio, next_index = max([(self.target_ratios[i] - self.ratios[i], i)
+                                if self.queues[i] or (self.batch_eval and self.non_evaluated_actions[i]) else (-inf, i)
+                                for i in range(len(self.ratios))], key=lambda item: item[0])
+
+        if prio == -inf:
+            # only bad actions left to ground
+            action = self.bad_actions.pop()
+            self.closed.append(action)
+            schema = action.predicate.name
+            self.num_grounded_bad_actions[schema] += 1
+            self.num_grounded_actions[self.schemas.index(schema)] += 1
+            return action
 
         if self.batch_eval and self.non_evaluated_actions[next_index]:
             actions = self.non_evaluated_actions[next_index]
